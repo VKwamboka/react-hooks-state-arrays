4e77e9b4fdb2a167fb1e97b4e8f5a4f1df187a61
verah <verahnyagaka101@gmail.com>
2022-07-14 07:51:23 +0300

    Assignment completed

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -44,22 +44,29 @@ function SpicyFoodList() {
     </div>
   );
 
-  
-
-  // const [filterBy, setFilterBy] = useState("All");
   // function handleFilterChange(event) {
+  // const [filterBy, setFilterBy] = useState("All");
+    
+  //   const foodsToDisplay = foods.filter((food) => {
+  //     if (filterBy === "All") {
+  //       return true;
+  //     } else {
+  //       return food.cuisine === filterBy;
+  //     }
+  //   });
   //   setFilterBy(event.target.value);
+  //   return (
+  //     <select name="filter" onChange={handleFilterChange}>
+  //       <option value="All">All</option>
+  //       <option value="American">American</option>
+  //       <option value="Sichuan">Sichuan</option>
+  //       <option value="Thai">Thai</option>
+  //       <option value="Mexican">Mexican</option>
+  //     </select>
+  //   );
+   
   // }
   
-  // return (
-  //   <select name="filter" onChange={handleFilterChange}>
-  //     <option value="All">All</option>
-  //     <option value="American">American</option>
-  //     <option value="Sichuan">Sichuan</option>
-  //     <option value="Thai">Thai</option>
-  //     <option value="Mexican">Mexican</option>
-  //   </select>
-  // );
 }
 
 export default SpicyFoodList;
